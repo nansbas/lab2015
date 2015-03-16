b642f486c6a9615e3ea866469f5b97ed22514f46
Richard Dong <richarddzh@gmail.com>
2015-03-16 18:17:01 +0800

    SOM learner for simple RF.

---
+++ LearnSimpleSOM.c
@@ -0,0 +1,76 @@
+#include <mex.h>
+#include <stdlib.h>
+#include "MyMexHelper.h"
+
+Matrix image, som, som2, neighbor;
+double rate;
+double * dist;
+double * sum;
+
+#define IMG(x,y) (((double*)(image.data))[(y)+(x)*image.h])
+#define SOM(x,y,z) (((double*)(som2.data))[(y)+(x)*som.h+(z)*som.h*som.w])
+#define NB(x,y) (((double*)(neighbor.data))[(y)+(x)*neighbor.h])
+#define DIST(x,y,z) (dist[(y)+(x)*som.h+(z)*som.h*som.w])
+#define SUM(x,y,z) (sum[(y)+(x)*som.h+(z)*som.h*som.w])
+
+void doBlock(int sx, int sy);
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+  int i, sx, sy;
+  if (!GetInputMatrix(nrhs, prhs, 0, mxDOUBLE_CLASS, &som)) return;
+  if (!GetInputMatrix(nrhs, prhs, 1, mxDOUBLE_CLASS, &image)) return;
+  if (!GetInputMatrix(nrhs, prhs, 2, mxDOUBLE_CLASS, &neighbor)) return;
+  if (!GetInputValue(nrhs, prhs, 3, &rate)) return;
+  som2 = som;
+  if (!GetOutputMatrix(nlhs, plhs, 0, &som2)) return;
+  for (i = 0; i < som.h * som.w * som.n; i++) {
+    ((double*)som2.data)[i] = ((double*)som.data)[i];
+  }
+  dist = (double *)malloc(sizeof(double) * som.h * som.w * som.n);
+  sum = (double *)malloc(sizeof(double) * som.h * som.w * som.n);
+  for (sx = 0; sx + som.w * 2 < image.w; sx++)
+    for (sy = 0; sy + som.h * 2 < image.h; sy++)
+      doBlock(sx, sy);
+  free(dist);
+  free(sum);
+}
+
+void doBlock(int sx, int sy)
+{
+  int x, y, i, dx, dy, px, py, mx, my, mi;
+  double d, pimg, psom, maxsum = -1, mindist = -1;
+  for (x = 0; x < som.w; x++)
+  for (y = 0; y < som.h; y++)
+  for (i = 0; i < som.n; i++) {
+    DIST(x,y,i) = 0;
+    SUM(x,y,i) = 0;
+    for (dx = 0; dx < som.w; dx++)
+    for (dy = 0; dy < som.h; dy++) {
+      px = sx + x + dx;
+      py = sy + y + dy;
+      pimg = IMG(px,py);
+      psom = SOM(dx,dy,i);
+      d = pimg - psom;
+      DIST(x,y,i) += d * d;
+      SUM(x,y,i) += psom * pimg; 
+    }
+    if (maxsum < SUM(x,y,i)) maxsum = SUM(x,y,i);
+    if (mindist < 0 || mindist > DIST(x,y,i)) {
+      mindist = DIST(x,y,i);
+      mx = x;
+      my = y;
+      mi = i;
+    }
+  }
+  if (SUM(mx,my,mi) * 2 < maxsum) return;
+  mexPrintf("SUM: %f\n", SUM(mx,my,mi));
+  for (x = 0; x < som.w; x++)
+  for (y = 0; y < som.h; y++)
+  for (i = 0; i < som.n; i++) {
+    SOM(x,y,i) += (IMG(sx+mx+x,sy+my+y) - SOM(x,y,i)) * NB(mi,i) * rate * SUM(mx,my,mi);
+  }
+}
+
+
+
---
+++ MyMexHelper.h
@@ -0,0 +1,50 @@
+typedef struct _matrix_ {
+  mwSize nDim, h, w, n;
+  const mwSize *dims;
+  void * data;
+  mxClassID classID;
+} Matrix;
+
+int GetInputMatrix(int nrhs, const mxArray *prhs[], int idx, mxClassID classID, Matrix * mat)
+{
+  const mxArray * arr = NULL;
+  int i = 0;
+  if (idx >= nrhs || idx < 0) {
+    mexErrMsgTxt("Not enough input arguments.");
+    return 0;
+  }
+  arr = prhs[idx];
+  mat->nDim = mxGetNumberOfDimensions(arr);
+  mat->dims = mxGetDimensions(arr);
+  mat->classID = mxGetClassID(arr);
+  if (classID != mat->classID) {
+    mexErrMsgTxt("Not expected type of arguments.");
+    return 0;
+  }
+  mat->data = mxGetData(arr);
+  if (mat->nDim > 0) mat->h = mat->dims[0];
+  if (mat->nDim > 1) mat->w = mat->dims[1];
+  for (i = 2, mat->n = 1; i < mat->nDim; i++) mat->n *= mat->dims[i];
+  return 1;
+}
+
+int GetInputValue(int nrhs, const mxArray *prhs[], int idx, double * val) 
+{
+  if (idx >= nrhs || idx < 0) {
+    mexErrMsgTxt("Not enough input arguments.");
+    return 0;
+  }
+  *val = mxGetScalar(prhs[idx]);
+  return 1; 
+}
+
+int GetOutputMatrix(int nlhs, mxArray *plhs[], int idx, Matrix * mat)
+{
+  if (idx != 0 && (idx >= nlhs || idx < 0)) {
+    mexErrMsgTxt("Not enough input arguments.");
+    return 0;
+  }
+  plhs[idx] = mxCreateNumericArray(mat->nDim, mat->dims, mat->classID, mxREAL);
+  mat->data = mxGetData(plhs[idx]);
+  return 1;
+}
