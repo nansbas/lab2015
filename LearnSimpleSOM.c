dcc3a12f3ab08b858473436e444ad0abbd59cdc7
Richard Dong <richarddzh@gmail.com>
2015-03-17 14:43:35 +0800

    script to learn simple RF from images

--- LearnSimpleSOM.c
+++ LearnSimpleSOM.c
@@ -29,8 +29,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   }
   dist = (double *)malloc(sizeof(double) * som.h * som.w * som.n);
   sum = (double *)malloc(sizeof(double) * som.h * som.w * som.n);
-  for (sx = 0; sx + som.w * 2 < image.w; sx++)
-    for (sy = 0; sy + som.h * 2 < image.h; sy++)
+  for (sx = 0; sx + som.w * 2 < image.w; sx += 3)
+    for (sy = 0; sy + som.h * 2 < image.h; sy += 3)
       doBlock(sx, sy);
   free(dist);
   free(sum);
@@ -64,7 +64,7 @@ void doBlock(int sx, int sy)
     }
   }
   if (SUM(mx,my,mi) * 2 < maxsum) return;
-  mexPrintf("SUM: %f\n", SUM(mx,my,mi));
+  if (SUM(mx,my,mi) > 1) mexPrintf("SUM: %f\n", SUM(mx,my,mi));
   for (x = 0; x < som.w; x++)
   for (y = 0; y < som.h; y++)
   for (i = 0; i < som.n; i++) {
---
+++ RunLearnSimpleSOM.m
@@ -0,0 +1,26 @@
+folder = '../../ethz-jpg/';
+files = dir([folder, '*.jpg']);
+load som.mat;
+k = [-1,-1,-1;-1,8,-1;-1,-1,-1]/8;
+out = ones(79,79);
+for i = 1:length(files)
+    fprintf('%d: file %s\n', i, files(i).name);
+    img = imread([folder, files(i).name]);
+    if size(img,3) == 3
+        img = rgb2gray(img);
+    end
+    if size(img,3) > 1
+        img = img(:,:,1);
+    end
+    img = abs(imfilter(img, k, 'replicate'));
+    img = double(img)/double(max(img(:)))*1.5;
+    img(img>1) = 1;
+    som = LearnSimpleSOM(som, img, neighbor, 0.001);
+    for j = 1:size(som,3)
+       x = mod(j-1,8);
+       y = (j-x-1)/8;
+       out((1:9)+y*10, (1:9)+x*10) = som(:,:,j);
+    end
+    imwrite(out, 'som.png');
+    save('som.mat', 'som', 'neighbor');
+end
