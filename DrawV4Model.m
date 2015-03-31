201685d9aee858531c166e5c42047ddab4ae387e
Richard Dong <richarddzh@gmail.com>
2015-03-31 15:13:40 +0800

    som training of shape category

--- DrawV4Model.m
+++ DrawV4Model.m
@@ -1,11 +1,20 @@
 function DrawV4Model(model)
   %figure
   hold on
+  if size(model,2) <= 5
+    model = model(:,[2,3,1,4,5]);
+    radius = 0.05;
+    rects = model(:,1:2) - radius;
+    line1 = [model(:,1),model(:,1)+cos(model(:,4))*radius,model(:,2),model(:,2)+sin(model(:,4))*radius];
+    line2 = [model(:,1),model(:,1)+cos(model(:,5))*radius,model(:,2),model(:,2)+sin(model(:,5))*radius];
+  else
+    radius = mean(model(:,4)) / 3;
+    rects = model(:,5:6) - radius;
+    line1 = [model(:,5),model(:,5)+cos(model(:,9))*radius,model(:,6),model(:,6)+sin(model(:,9))*radius];
+    line2 = [model(:,5),model(:,5)+cos(model(:,12))*radius,model(:,6),model(:,6)+sin(model(:,12))*radius];
+  end
+  model(:,3) = model(:,3) - min(model(:,3));
   maxColor = max(model(:,3));
-  radius = mean(model(:,4)) / 3;
-  rects = model(:,5:6) - radius;
-  line1 = [model(:,5),model(:,5)+cos(model(:,9))*radius,model(:,6),model(:,6)+sin(model(:,9))*radius];
-  line2 = [model(:,5),model(:,5)+cos(model(:,12))*radius,model(:,6),model(:,6)+sin(model(:,12))*radius];
   [~,idx] = sort(model(:,3));
   for i = idx'
     rectangle(...
--- LearnV4.m
+++ LearnV4.m
@@ -1,19 +1,19 @@
-function model = LearnV4(category)
+function category = LearnV4(category)
+  %{
   [rf,out] = MakeSimpleRF(9, 0:5:175, [6,6]);
-  model = [];
-  fp = fopen([category.category, '.txt'], 'w');
+  sample = [];
   for i = 1:length(category.files)
+    fprintf('%s: %d: %s\n', category.category, i, category.files(i).name);
     img = imread(['D:\Downloads\ethz_shape_classes_v12\',category.category,'\',category.files(i).name,'.jpg']);
     [out,ori,ridge,lmap,lines,graph,v4] = SimpleCell(img, rf);
     for j = 1:size(category.files(i).groundtruth,1)
-      sample = GetInRect(v4, category.files(i).groundtruth(j,:));
-      model = [model; sample(:,[3,5,6,9,12])];
-      fprintf(fp, '%d\n', size(sample,1));
-      fprintf(fp, '%f \t%f \t%f \t%d \t%d\n', sample(:,[3,5,6,9,12])');
+      aSample = GetInRect(v4, category.files(i).groundtruth(j,:));
+      sample = cat(1, sample, aSample(:,[3,5,6,9,12]));
     end
   end
-  fprintf(fp, '0\n');
-  fclose(fp);
+  category.sample = sample;
+  %}
+  category.model = LearnV4SOM(category.model, category.sample, 0.01, 0.02);
 end
 
 function sample = GetInRect(v4, rect)
@@ -22,6 +22,4 @@ function sample = GetInRect(v4, rect)
   sample(:,5) = (sample(:,5) - rect(1)) / (rect(3)-rect(1));
   sample(:,6) = (sample(:,6) - rect(2)) / (rect(4)-rect(2));
   sample(:,3) = sample(:,3) / max(sample(:,3));
-  sample(:,9) = round(sample(:,9)/pi*180);
-  sample(:,12) = round(sample(:,12)/pi*180);
 end
