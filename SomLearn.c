b3d07021486fa93cfab1f07d995be5fad378fe80
Richard Dong <richarddzh@gmail.com>
2015-05-11 15:43:38 +0800

    som layer

--- SomLearn.c
+++ SomLearn.c
@@ -1,15 +1,22 @@
 #include <mex.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 #include "MyMexHelper.h"
 
-Matrix ridge, ori, cell;
+Matrix ridge, ori, cell, v4pos;
 double minRidge, oriFactor, neighbor;
+int * map;
+int adjx[4] = {-1,-1, 0, 1};
+int adjy[4] = { 0,-1,-1,-1};
 
 #define MAX_CELLS 1000
 #define C(r,c) (((double*)(cell.data))[(r)+(c)*cell.h])
 #define R(x,y,z) (((double*)(ridge.data))[(y)+(x)*ridge.h+(z)*ridge.h*ridge.w])
 #define ORI(x,y,z) (((double*)(ori.data))[(y)+(x)*ridge.h+(z)*ridge.h*ridge.w])
+#define M(x,y) (map[(y)+(x)*ridge.h])
+#define V4P(i,j,k) (((double*)(v4pos.data))[(i)+(j)*cell.h+(k)*v4pos.h])
 
 struct _cell_t_ {
   double x, y, sina, cosa, w, out, nw;
@@ -17,7 +24,7 @@ struct _cell_t_ {
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  int x, y, i, j, k;
+  int x, y, i, j, k, ax, ay, ak;
   double w, d, minD, o, cx, cy;
   if (!GetInOutMatrix(nrhs, prhs, 0, nlhs, plhs, 0, mxDOUBLE_CLASS, &cell)) return;
   if (!GetInputMatrix(nrhs, prhs, 1, mxDOUBLE_CLASS, &ridge)) return;
@@ -60,17 +67,20 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     cells[k].w += w;
     cells[k].out += w * exp(-minD / 80);
   }
+  if (neighbor > 0)
   for (i = 0; i < cell.h; i++)
   for (j = 0; j < cell.h; j++) {
     w = C(i,j+4);
     if (w == 0) continue;
+    if (cells[j].w == 0) continue;
     cells[i].nw += w * neighbor;
-    cells[i].x += (w * neighbor) * cells;
+    cells[i].x += (w * neighbor) * cells[j].x / cells[j].w;
+    cells[i].y += (w * neighbor) * cells[j].y / cells[j].w;
   }
   for (i = 0; i < cell.h; i++) {
-    if (cells[i].w == 0) continue;
-    cells[i].x /= cells[i].w;
-    cells[i].y /= cells[i].w;
+    if (cells[i].w + cells[i].nw == 0 ) continue;
+    cells[i].x /= cells[i].w + cells[i].nw;
+    cells[i].y /= cells[i].w + cells[i].nw;
     o = atan2(cells[i].sina, cells[i].cosa) / M_PI * 180;
     if (o < 0) o += 360;
     if (o > 360) o = 360;
@@ -79,5 +89,60 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     C(i,2) = o / 2;
     C(i,3) = cells[i].out;
   }
+  v4pos.w = 2;
+  v4pos.h = cell.h * cell.h;
+  v4pos.n = 1;
+  v4pos.classID = mxDOUBLE_CLASS;
+  v4pos.dims = NULL;
+  if (!GetOutputMatrix(nlhs, plhs, 1, &v4pos)) return;
+  for (i = 0; i < cell.h; i++)
+  for (j = 0; j < cell.h; j++) {
+    C(i,j+4) = 0;
+  }
+  map = (int *)malloc(sizeof(int) * ridge.h * ridge.w);
+  for (i = 0; i < ridge.n; i++) {
+    for (y = 0; y < ridge.h; y++)
+    for (x = 0; x < ridge.w; x++) {
+      w = R(x,y,i);
+      o = ORI(x,y,i);
+      if (w <= minRidge) continue;
+      k = -1;
+      for (j = 0; j < cell.h; j++) {
+        d = o - C(j,2);
+        cx = C(j,0);
+        cy = C(j,1);
+        if (d < 0) d = -d;
+        if (d > 90) d = 180 - d;
+        d = oriFactor * d * d + (cx - x) * (cx - x) + (cy - y) * (cy - y);
+        if (k < 0 || d < minD) {
+          k = j;
+          minD = d;
+        }
+      }
+      M(x,y) = k;
+      for (j = 0; j < 4; j++) {
+        ax = adjx[j] + x;
+        ay = adjy[j] + y;
+        if (ax < 0 || ay < 0 || ax >= ridge.w || ay >= ridge.h) continue;
+        if (R(ax,ay,i) <= minRidge) continue;
+        ak = M(ax,ay);
+        if (ak == k) continue;
+        w += R(ax,ay,i);
+        C(k,ak+4) += w;
+        C(ak,k+4) += w;
+        V4P(k,ak,0) += w * (x + ax) / 2;
+        V4P(k,ak,1) += w * (y + ay) / 2;
+        V4P(ak,k,0) += w * (x + ax) / 2;
+        V4P(ak,k,1) += w * (y + ay) / 2;
+      }
+    }
+  }
+  for (i = 0; i < cell.h; i++)
+  for (j = 0; j < cell.h; j++) {
+    if (C(i,j+4) <= 0) continue;
+    V4P(i,j,0) /= C(i,j+4);
+    V4P(i,j,1) /= C(i,j+4);
+  }
+  free(map);
 }
 
--- SomModel.m
+++ SomModel.m
@@ -1,7 +1,26 @@
+function [som,v4pos] = SomModel(action, som, arg1, arg2)
+  if (strcmp(action,'init')) 
+    som = InitModel(8, arg1, arg2);
+  elseif (strcmp(action,'learn'))
+    [som,v4pos] = SomLearn(som, arg1, arg2, 0.8, 1, 0.1);
+  elseif (strcmp(action,'draw'))
+    DrawComplexCell(som);
+  end
+end
+
+function som = InitModel(nw, width, height)
+  [x,y] = meshgrid(1:nw);
+  som = [x(:)/nw*width,y(:)/nw*height,rand(nw*nw,1)*180,ones(nw*nw,1)];
+  x = repmat(x(:), 1, nw*nw);
+  y = repmat(y(:), 1, nw*nw);
+  d = (x-x').^2 + (y-y').^2;
+  som = [som,d<=2&d>0];
+end
+
 function DrawComplexCell(cells)
   r1 = 5;
   r2 = 3;
-  cells(:,4) = cells(:,4) / max(cells(:,4)) * 0.7 + 0.3;
+  cells(:,4) = cells(:,4) / max(cells(:,4));
   x1 = cells(:,1) - r2 * cos(cells(:,3)/180*pi);
   x2 = cells(:,1) + r2 * cos(cells(:,3)/180*pi);
   y1 = cells(:,2) + r2 * sin(cells(:,3)/180*pi);
