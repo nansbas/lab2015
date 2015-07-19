748c1de74e758c485ec9a0d05b074f2dec9c7a95
ZhengDong <zhengdong@ZhengDongdeiMac.local>
2015-07-19 17:59:59 +0800

    finish detection 1,2,4,5

--- TempScript.m
+++ TempScript.m
@@ -1,6 +1,7 @@
-result = [];
-runCat = 2;
-for i=1:5
+%% Run detection.
+for i=[4,5]
+  runCat = i;
+  result = [];
   for j=1:length(ethzv4(i).files)
     [r,jj]=FindV4ModelInImage(ethzv4(runCat).cluster,ethzv4(runCat).model,ethzv4(i).files(j));
     if i~=runCat && ~isempty(jj)
@@ -12,7 +13,25 @@ for i=1:5
     result=[result;jj];
     fprintf('ok: %d, %d\n', i, j);
   end
+  ethzv4(i).result = result;
 end
+allpos = [44,55,91,66,33];
+[~,idx] = sort(result(:,8)/max(result(:,8))-result(:,7));
+r = result(idx,:);
+for i = 1:size(r,1)
+  r(i,1) = sum(r(1:i,9)<=0.1)/255;
+  r(i,2) = sum(r(1:i,9)>0.1)/allpos(runCat);
+end
+[~,idx] = unique(r(:,1));
+idx2 = 1;
+for i = 2:length(idx)
+  idx2 = [idx2, idx(i)-1, idx(i)];
+end
+idx2 = [idx2, size(r,1)];
+r = r(idx2,1:2);
+close all
+plot(r(:,1),r(:,2));
+%
 %% Find where can set empty label.
 %{
 for i = [1,2,4,5]
