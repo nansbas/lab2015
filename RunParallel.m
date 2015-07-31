14929485aa19bd40ab87406cad4f8d75c9eca574
ZhengDong <zhengdong@ZhengDongdeiMac.local>
2015-07-31 15:28:04 +0800

    run parallel on test images

--- RunParallel.m
+++ RunParallel.m
@@ -1,9 +1,13 @@
 function RunParallel(ethzv4)
   pool = parpool(4);
-  for runCat = [4,5]
+  for runCat = 4
     r = {};
-    parfor j = 1:5
-      r{j} = RunCategory(ethzv4, runCat, j);
+    for testCat = 1:5
+      r1 = {};
+      parfor j = 1:length(ethzv4(testCat).files)
+        r1{j} = RunCategory(ethzv4, runCat, testCat, j);
+      end
+      r{testCat} = vertcat(r1{:});
     end
     result = vertcat(r{:});
     ethzv4(runCat).result = result;
@@ -27,18 +31,16 @@ function RunParallel(ethzv4)
   delete(pool);
 end
 
-function f = RunCategory(ethzv4, runCat, testCat)
-  result = [];
+function result = RunCategory(ethzv4, runCat, testCat, testImg)
   i = testCat;
-  for j=1:length(ethzv4(i).files)
-    [r,jj,maxr]=FindV4ModelInImage(ethzv4(runCat).cluster,ethzv4(runCat).model,ethzv4(i).files(j));
-    if i~=runCat && ~isempty(jj)
-      jj(:,7) = 0;
-    end
-    if ~isempty(jj)
-      jj = [repmat(i,size(jj,1),1),repmat(j,size(jj,1),1),jj];
-    end
-    result=[result;jj];
-    fprintf('Runcat=%d, ok: %d, %d, max=%d\n', runCat, i, j, maxr);
+  j = testImg;
+  [r,jj,maxr] = FindV4ModelInImage(ethzv4(runCat).cluster,ethzv4(runCat).model,ethzv4(i).files(j));
+  if i~=runCat && ~isempty(jj)
+    jj(:,7) = 0;
   end
+  if ~isempty(jj)
+    jj = [repmat(i,size(jj,1),1),repmat(j,size(jj,1),1),jj];
+  end
+  result = jj;
+  fprintf('Runcat=%d, ok: %d, %d, max=%d\n', runCat, i, j, maxr);
 end
--- TempScript.m
+++ TempScript.m
@@ -54,8 +54,8 @@ for i = 1:12
 end
 %}
 %% learn shape model.
-%{
-for i=1:5
+%
+for i=4
   [c,dist,label,maxZero,x,y,s,d,a,n,ignore] = LearnV4ShapeModel(ethzv4(i).files, ethzv4(i).model.init, ethzv4(i).model.sampleIndex);
   ethzv4(i).cluster.c = c;
   ethzv4(i).cluster.d = dist;
@@ -68,43 +68,6 @@ for i=1:5
   ethzv4(i).model.a = a;
   ethzv4(i).model.n = n;
   ethzv4(i).model.ignore = ignore;
-end
-%}
-%% Run detection.
-%
-for runCat = [1,2,5,4,3]
-result = [];
-for i=1:5
-  for j=1:length(ethzv4(i).files)
-    [r,jj,maxr]=FindV4ModelInImage(ethzv4(runCat).cluster,ethzv4(runCat).model,ethzv4(i).files(j));
-    if i~=runCat && ~isempty(jj)
-      jj(:,7) = 0;
-    end
-    if ~isempty(jj)
-      jj = [repmat(i,size(jj,1),1),repmat(j,size(jj,1),1),jj];
-    end
-    result=[result;jj];
-    fprintf('Runcat=%d, ok: %d, %d, max=%d\n', runCat, i, j, maxr);
-  end
-end
-ethzv4(runCat).result = result;
-allpos = [44,55,91,66,33];
-[~,idx] = sort(result(:,8)/max(result(:,8))-result(:,7));
-r = result(idx,:);
-for i = 1:size(r,1)
-  r(i,1) = sum(r(1:i,9)<=0.4)/255;
-  r(i,2) = sum(r(1:i,9)>0.4)/allpos(runCat);
-end
-[~,idx] = unique(r(:,1));
-idx2 = 1;
-for i = 2:length(idx)
-  idx2 = [idx2, idx(i)-1, idx(i)];
-end
-idx2 = [idx2, size(r,1)];
-r = r(idx2,1:2);
-close all
-plot(r(:,1),r(:,2));
-ethzv4(runCat).fppi = r;
 end
 %}
 %% Find where can set empty label.
