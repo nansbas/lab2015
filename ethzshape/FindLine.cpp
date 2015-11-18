d3b2eb0ca6bb372dc6c1c55980a952866728dc83
Richard Dong <richarddzh@gmail.com>
2015-11-18 12:42:16 +0800

    rewrite findline with c++

--- ethzshape/FindLine.cpp
+++ ethzshape/FindLine.cpp
@@ -1,21 +1,20 @@
 #include "MyMexHelper.hpp"
-#include <map>
 
 // Input Params
 Matrix<double> in; 
-int minLength; 
+int minLength;
+double minValue;
 
 // Output Params
 Matrix<int> ridge;
 Matrix<int> lmap;
-Matrix<int> pmap;
-CellMatrix lcell;
-std::vector<Matrix<int> > lines;
+CellMatrix lines;
 
 // Find Ridge
 void findRidge() {
   for (int i = 1; i + 1 < in.h; i++) for (int j = 1; j + 1 < in.w; j++) {
     double ij = in(i,j);
+    if (ij <= minValue) continue;
     if ((ij >= in(i-1,j) && ij > in(i+1,j)) || (ij >= in(i,j-1) && ij > in(i,j+1)))
       ridge(i,j) = 1;
   }
@@ -62,23 +61,20 @@ struct Tree {
     for (i = q.back(); i >= 0; i = t[i].link) line.push_back(i);
   }
   void MapLine() {
-    lines.push_back(Matrix<int>());
-    Matrix<int> & l = lines.back();
-    l.CreateArray(MatrixBase::MakeDims(line.size(), 2), mxINT32_CLASS);
-    int lidx = lines.size();
+    Matrix<int> & l = lines.Append<int>(MatrixBase::MakeDim(line.size(), 2), mxINT32_CLASS);
+    int lidx = lines.Size();
     for (int i = 0; i < t.size(); i++) ridge(t[i].y,t[i].x) = 1;
     for (int i = 0; i < line.size(); i++) {
       int k = line[i];
       ridge(t[k].y,t[k].x) = 2;
       lmap(t[k].y,t[k].x) = lidx;
-      pmap(t[k].y,t[k].x) = i + 1;
       l(i,0) = t[k].x + 1;
       l(i,1) = t[k].y + 1;
     }
   }
 } tree;
+
 void findLine() {
-  lines.clear();
   for (int y = 0; y < in.h; y++) for (int x = 0; x < in.w; x++) {
     if (ridge(y,x) != 1) continue;
     tree.Clear();
@@ -97,26 +93,16 @@ void findLine() {
   }
 }
 
-// Connect Lines
-struct Graph {
-  struct Link {
-    int fromIdx, toIdx, dist;
-  };
-  std::map<int,> link;
-};
-
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  double dMinLength;
   MatrixBase::Bind(nlhs, plhs, nrhs, prhs);
   if (!in.SetInput(0, mxDOUBLE_CLASS)) return;
   if (!Matrix<int>::GetInputValue(1, minLength)) return;
-  if (!Matrix<int>::GetInputValue(2, maxGap))
+  if (!Matrix<double>::GetInputValue(2, minValue)) return;
   if (!ridge.SetOutput(0, in.dims, mxINT32_CLASS)) return;
   if (!lmap.SetOutput(1, in.dims, mxINT32_CLASS)) return;
-  if (!pmap.SetOutput(2, in.dims, mxINT32_CLASS)) return;
+  if (!lines.SetOutput(2)) return;
   findRidge();
   findLine();
-  if (!lcell.SetOutput(3, MatrixBase::MakeDims(lines.size()), mxCELL_CLASS)) return;
-  for (int i = 0; i < lines.size(); i++) lcell.Set(i, lines[i]);
+  lines.Finish();
 }
--- ethzshape/MyMexHelper.hpp
+++ ethzshape/MyMexHelper.hpp
@@ -2,31 +2,45 @@
 #include <vector>
 
 struct MatrixBase {
-  mwSize nDim, h, w, n;
-  mxClassID classID;
-  std::vector<mwSize> dims;
-  mxArray * arr;
+  
   static int nlhs;
   static int nrhs;
   static mxArray ** plhs;
   static const mxArray ** prhs;
-  static void Bind(int nl, mxArray ** pl, int nr, const mxArray ** pr) {
+  
+  static void Bind(int nl, mxArray ** pl, int nr, const mxArray ** pr)
+  {
     nlhs = nl; plhs = pl; nrhs = nr; prhs = pr;
   }
+
+  static std::vector<mwSize> MakeDim(size_t i, size_t j)
+  {
+    std::vector<mwSize> v;
+    v.push_back((mwSize)i);
+    v.push_back((mwSize)j);
+    return v;
+  }
+
+  static std::vector<mwSize> MakeDim(size_t i, size_t j, size_t k)
+  {
+    std::vector<mwSize> v;
+    v.push_back((mwSize)i);
+    v.push_back((mwSize)j);
+    v.push_back((mwSize)k);
+    return v;
+  }
+
+  mwSize nDim, h, w, n;
+  mxClassID classID;
+  std::vector<mwSize> dims;
+  mxArray * arr;
   virtual void GetDataPointer() = 0;
-  MatrixBase():arr(NULL){}
-  static std::vector<mwSize> MakeDims(size_t i) {
-    std::vector<mwSize> d;
-    d.push_back((mwSize)i);
-    return d;
-  }
-  static std::vector<mwSize> MakeDims(size_t i, size_t j) {
-    std::vector<mwSize> d;
-    d.push_back((mwSize)i);
-    d.push_back((mwSize)j);
-    return d;
-  }
-  void SetDims(const std::vector<mwSize> & d) {
+
+  MatrixBase():arr(NULL),h(0),w(0),n(0),nDim(0) {}
+  virtual ~MatrixBase() {}
+  
+  void SetDim(const std::vector<mwSize> & d)
+  {
     dims = d;
     nDim = (mwSize) dims.size();
     h = w = 0; n = 1;
@@ -36,22 +50,22 @@ struct MatrixBase {
       if (i > 1) n *= dims[i];
     }
   }
-  void LoadArray(const mxArray * a) {
+
+  void LoadArray(const mxArray * a)
+  {
     nDim = mxGetNumberOfDimensions(a);
     const mwSize * d = mxGetDimensions(a);
-    classID = mxGetClassID(a);
     dims.clear();
-    h = w = 0; n = 1;
-    for (int i = 0; i < nDim; i++) {
-      if (i == 0) h = d[0];
-      if (i == 1) w = d[1];
-      if (i > 1) n *= d[i];
-      dims.push_back(d[i]); 
-    }
+    for (int i = 0; i < nDim; i++)
+      dims.push_back(d[i]);
     arr = (mxArray *) a;
+    classID = mxGetClassID(a);
+    SetDim(dims);
     GetDataPointer();
   }
-  mxArray * CreateArray() {
+
+  mxArray * CreateArray()
+  {
     if (classID == mxCELL_CLASS) {
       arr = mxCreateCellArray(nDim, &(dims[0]));
     } else if (classID == mxLOGICAL_CLASS) {
@@ -62,12 +76,9 @@ struct MatrixBase {
     GetDataPointer();
     return arr;
   }
-  mxArray * CreateArray(const std::vector<mwSize> & d, mxClassID cid) {
-    classID = cid;
-    SetDims(d);
-    return CreateArray();
-  }
-  bool SetInput(int idx, mxClassID cid) {
+
+  bool SetInput(int idx, mxClassID cid)
+  {
     if (idx >= nrhs || idx < 0) {
       mexErrMsgTxt("Not enough input arguments.");
       return false;
@@ -79,20 +90,21 @@ struct MatrixBase {
     }
     return true;
   }
-  bool SetOutput(int idx, std::vector<mwSize> d, mxClassID cid) {
-    dims = d;
+
+  bool SetOutput(int idx, const std::vector<mwSize> & d, mxClassID cid)
+  {
     classID = cid;
-    return SetOutput(idx);
-  }
-  bool SetOutput(int idx) {
+    SetDim(d);
     if (idx != 0 && (idx >= nlhs || idx < 0)) {
       mexErrMsgTxt("Not enough output arguments.");
       return false;
     }
-    plhs[idx] = CreateArray(dims, classID);
+    plhs[idx] = CreateArray();
     return true;
   }
-  bool SetInOut(int iIn, int iOut, mxClassID cid) {
+
+  bool SetInOut(int iIn, int iOut, mxClassID cid)
+  {
     if (!SetInput(iIn, cid)) return false;
     if (iOut != 0 && (iOut >= nlhs || iOut < 0)) {
       mexErrMsgTxt("Not enough output arguments.");
@@ -103,22 +115,30 @@ struct MatrixBase {
     GetDataPointer();
     return true;
   }
-  void DestroyArray() {
+
+  void DestroyArray()
+  {
     if (arr) {
       mxDestroyArray(arr);
       arr = NULL;
     }
   }
+
 };
 
 template <typename T>
 struct Matrix : public MatrixBase {
+
   T * data;
+
   inline T & operator() (int i, int j, int k) {
     return data[i + j * h + k * h * w];
   }
   inline T & operator() (int i, int j) {
     return data[i + j * h];
+  }
+  inline T & operator() (int i) {
+    return date[i];
   }
   inline const T & operator() (int i, int j, int k) const {
     return data[i + j * h + k * h * w];
@@ -126,7 +146,12 @@ struct Matrix : public MatrixBase {
   inline const T & operator() (int i, int j) const {
     return data[i + j * h];
   }
-  virtual void GetDataPointer() {
+  inline const T & operator() (int i) const {
+    return data[i];
+  }
+
+  virtual void GetDataPointer()
+  {
     if (classID == mxLOGICAL_CLASS) {
       data = (T *) mxGetLogicals(arr);
     } else if (classID == mxCELL_CLASS) {
@@ -135,15 +160,9 @@ struct Matrix : public MatrixBase {
       data = (T *) mxGetData(arr);
     }
   }
-  void NewData(const std::vector<mwSize> & d) {
-    SetDims(d);
-    data = new T[h * w * n]();
-  }
-  void DeleteData() {
-    delete[] data;
-    data = NULL;
-  }
-  static bool GetInputValue(int idx, T & value) {
+
+  static bool GetInputValue(int idx, T & value)
+  {
     if (idx >= nrhs || idx < 0) {
       mexErrMsgTxt("Not enough input arguments.");
       return false;
@@ -152,12 +171,81 @@ struct Matrix : public MatrixBase {
     value = (T) dValue;
     return true;
   }
+
 };
 
-struct CellMatrix : public Matrix<mxArray *> {
-  void Set(int i, const MatrixBase & m) {
-    mxSetCell(arr, i, m.arr);
+struct CellMatrix : public Matrix<MatrixBase *> {
+
+  std::vector<MatrixBase *> mpdata;
+  int pidx;
+
+  virtual ~CellMatrix()
+  {
+    ClearMatrixData();
   }
+
+  virtual void GetDataPointer()
+  {
+    if (mpdata.size() > 0)
+      data = &(mpdata[0]);
+    else
+      data = NULL;
+  }
+
+  void ClearMatrixData()
+  {
+    for (int i = 0; i < mpdata.size(); i++) {
+      if (mpdata[i] == NULL) continue;
+      delete mpdata[i];
+    }
+    mpdata.clear();
+  }
+  
+  bool SetOutput(int idx)
+  {
+    classID = mxCELL_CLASS;
+    if (idx != 0 && (idx >= nlhs || idx < 0)) {
+      mexErrMsgTxt("Not enough output arguments.");
+      return false;
+    }
+    arr = NULL;
+    ClearMatrixData();
+    pidx = idx;
+    return true;
+  }
+
+  template <typename T>
+  Matrix<T> & Append(const std::vector<mwSize> & d, mxClassID cid)
+  {
+    Matrix<T> * mp = new Matrix<T>();
+    mpdata.push_back(mp);
+    mp->SetDim(d);
+    mp->classID = cid;
+    mp->CreateArray();
+    GetDataPointer();
+    return (*mp);
+  }
+
+  size_t Size() 
+  {
+    return mpdata.size();
+  }
+
+  void Finish()
+  {
+    SetDim(MakeDim(1, mpdata.size()));
+    classID = mxCELL_CLASS;
+    if (pidx != 0 && (pidx >= nlhs || pidx < 0)) {
+      mexErrMsgTxt("Not enough output arguments.");
+      return;
+    }
+    plhs[pidx] = CreateArray();
+    for (int i = 0; i < mpdata.size(); i++) {
+      if (mpdata[i] == NULL) continue;
+      mxSetCell(arr, i, mpdata[i]->arr);
+    }
+  }
+
 };
 
 int MatrixBase::nlhs;
