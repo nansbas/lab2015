859f8520c03c8cc5d63d701aa69b3668e6099d82
Richard Dong <richarddzh@gmail.com>
2016-03-08 18:24:35 +0800

    extract V4 feature

--- caltech2016/V4LocalFeature.m
+++ caltech2016/V4LocalFeature.m
@@ -1,11 +1,23 @@
-function [enty,acty] = V4LocalFeature(img)
-  scale = 27; %[9,13,19,27];
+function [frame,enty,acty] = V4LocalFeature(img)
+  scale = 9;%[9,13,19,27];
+  frame = [];
+  [xp,yp] = meshgrid(1:size(img,2),1:size(img,1));
   for s = scale
     [rf,~] = MakeSimpleRF(s,0:20:175,[3,3]);
-    [~,cout] = SimpleCell(img,rf,fspecial('gauss',ceil(s*1.2),s/4));
+    [~,cout] = SimpleCell(img,rf,fspecial('gauss',ceil(s*3),s*0.7));
     acty = max(cout,[],3);
     cout(cout<0.001) = 0.001;
     p = cout ./ repmat(sum(cout,3),[1,1,size(rf,3)]);
     enty = -sum(p.*log(p),3);
+    mact = imdilate(acty, strel('square',s));
+    p = enty<mean(enty(:)) & acty>mean(acty(:)) & acty==mact;
+    frame = cat(1, frame, [xp(p),yp(p),p(p)*s]);
   end
+  close all;
+  imshow(img);
+  hold on;
+  for i = 1:size(frame,1)
+    rectangle('position',[frame(i,1:2)-frame(i,3),frame(i,[3,3])*2],'curvature',[1,1],'edgecolor',[0,1,0],'linewidth',1);
+  end
+  hold off;
 end
