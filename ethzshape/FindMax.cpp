e4c53732ab27ad0dfc950bd507232175e4c980e7
Richard Dong <richarddzh@gmail.com>
2016-03-10 10:11:28 +0800

    fix bug in FindMax

--- ethzshape/FindMax.cpp
+++ ethzshape/FindMax.cpp
@@ -6,7 +6,8 @@
 
 #include "MyMexHelper.hpp"
 
-Matrix<double> in, maxv;
+Matrix<double> in;
+Matrix<double> maxv;
 Matrix<mxLogical> maxm;
 int neighbor;
 
@@ -17,8 +18,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   MatrixBase::Bind(nlhs, plhs, nrhs, prhs);
   if (!in.SetInput(0, mxDOUBLE_CLASS)) return;
   if (!Matrix<int>::GetInputValue(1, neighbor)) return;
-  if (!maxm.SetOutput(0, in.dims, mxDOUBLE_CLASS)) return;
-  if (!maxv.SetOutput(1, in.dims, mxLOGICAL_CLASS)) return;
+  if (!maxv.SetOutput(0, in.dims, mxDOUBLE_CLASS)) return;
+  if (!maxm.SetOutput(1, in.dims, mxLOGICAL_CLASS)) return;
   if (neighbor < 1) return;
   if (in.w <= neighbor * 2 || in.h <= neighbor * 2) return;
   findMaximum();
@@ -42,34 +43,33 @@ void findMaximum()
     }
     for (int j = 0; j < in.w; j++) {
       int a = j - neighbor, b = j + neighbor;
-      if (a < 0 || (g[a] < h[b] && b < in.w)) {}// maxv(i,j) = h[b]; maxm(i,j) = r[b]; }
-      else if (b >= in.w || (g[a] > h[b] && a >= 0)) {}// maxv(i,j) = g[a]; maxm(i,j) = t[a]; }
-      else {}// maxv(i,j) = g[a]; maxm(i,j) = 1; }
+      if (a < 0 || (g[a] < h[b] && b < in.w)) { maxv(i,j) = h[b]; maxm(i,j) = r[b]; }
+      else if (b >= in.w || (g[a] > h[b] && a >= 0)) { maxv(i,j) = g[a]; maxm(i,j) = t[a]; }
+      else { maxv(i,j) = g[a]; maxm(i,j) = 1; }
     }
   }
   h.resize(in.h); g.resize(in.h); r.resize(in.h); t.resize(in.h);
-  for (int j = neighbor; j + neighbor < in.w; j++) {
+  for (int j = 0; j < in.w; j++) {
     for (int i = 0; i < in.h; i++) {
       if (i % gap == 0 || maxv(i,j) > h[i-1]) { h[i] = maxv(i,j); r[i] = maxm(i,j); }
       else if (maxv(i,j) < h[i-1]) { h[i] = h[i-1]; r[i] = r[i-1]; }
       else { h[i] = maxv(i,j); r[i] = 1; }
     }
     for (int i = in.h - 1; i >= 0; i--) {
-      if (i % gap == gap - 1 || i == in.h - 1 || maxv(i,j) > g[i+1]) { g[j] = in(i,j); t[j] = maxm(i,j); }
-      else if (in(i,j) < g[j+1]) { g[j] = g[j+1]; t[j] = t[j+1]; }
-      else { g[j] = in(i,j); t[j] = 1; }
+      if (i % gap == gap - 1 || i == in.h - 1 || maxv(i,j) > g[i+1]) { g[i] = maxv(i,j); t[i] = maxm(i,j); }
+      else if (maxv(i,j) < g[i+1]) { g[i] = g[i+1]; t[i] = t[i+1]; }
+      else { g[i] = maxv(i,j); t[i] = 1; }
     }
     for (int i = 0; i < in.h; i++) {
-      int a = i - neighbor, b = j + neighbor;
-      if (a < 0 || (g[a] < h[b] && b < in.w)) {}// maxv(i,j) = h[b]; maxm(i,j) = r[b]; }
-      else if (b >= in.w || (g[a] > h[b] && a >= 0)) {}// maxv(i,j) = g[a]; maxm(i,j) = t[a]; }
-      else {}// maxv(i,j) = g[a]; maxm(i,j) = 1; }
+      int a = i - neighbor, b = i + neighbor;
+      if (a < 0 || (g[a] < h[b] && b < in.w)) { maxv(i,j) = h[b]; maxm(i,j) = r[b]; }
+      else if (b >= in.w || (g[a] > h[b] && a >= 0)) { maxv(i,j) = g[a]; maxm(i,j) = t[a]; }
+      else { maxv(i,j) = g[a]; maxm(i,j) = 1; }
     }
-  }/*
+  }
   for (int i = 0; i < in.h; i++) for (int j = 0; j < in.w; j++) {
     if (i < neighbor || j < neighbor || i + neighbor >= in.h || j + neighbor >= in.w) maxm(i,j) = 0;
-    else if (maxv(i,j) > in(i,j)) maxm(i,j) = 0;
-    else if (maxm(i,j)) maxm(i,j) = 0; 
-    else maxm(i,j) = 1;
-  }*/
+    else if (maxv(i,j) == in(i,j) && maxm(i,j) == 0) maxm(i,j) = 1;
+    else maxm(i,j) = 0;
+  }
 }
